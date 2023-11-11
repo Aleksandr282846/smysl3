cfe740b2c73f2a40d4ec594856defa9e93a7cea6
Александр Парамош <aleksandrparamos@MacBook-Air-Aleksandr.local>
2023-11-12 00:34:20 +0200

    end of second part

--- blog/tests.py
+++ blog/tests.py
@@ -1,10 +1,38 @@
 from django.test import TestCase
 from django.urls import resolve
 from blog.views import home_page
+from blog.models import Article
 from django.http import HttpRequest
+from datetime import datetime
 
 
 class HomePageTest(TestCase):
+
+    def test_home_page_displays_articles(self):
+        Article.objects.create(
+            title='title 1',
+            summary='summary 1',
+            full_text='full_text 1',
+            pubdate=datetime.now(),
+        )
+        Article.objects.create(
+            title='title 2',
+            summary='summary 2',
+            full_text='full_text 2',
+            pubdate=datetime.now(),
+        )
+
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf8')
+
+        self.assertIn('title 1', html)
+        self.assertIn('summary 1', html)
+        self.assertNotIn('full_text 1', html)
+
+        self.assertIn('title 2', html)
+        self.assertIn('summary 2', html)
+        self.assertNotIn('full_text 2', html)
 
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
@@ -18,3 +46,37 @@ class HomePageTest(TestCase):
         self.assertIn('<title>Сайт Алексея Куличевского</title>', html)
         self.assertIn('<h1>Алексей Куличевский</h1>', html)
         self.assertTrue(html.endswith('</html>'))
+
+
+class ArticleModelTest(TestCase):
+
+    def test_article_model_save_and_retrieve(self):
+        article1 = Article(
+            title='article 1',
+            full_text='full_text 1',
+            summary='summary 1',
+            category='category 1',
+            pubdate=datetime.now(),
+        )
+        article1.save()
+
+        article2 = Article(
+            title='article 2',
+            full_text='full_text 2',
+            summary='summary 2',
+            category='category 2',
+            pubdate=datetime.now(),
+        )
+        article2.save()
+
+        all_articles = Article.objects.all()
+
+        self.assertEqual(len(all_articles), 2)
+        self.assertEqual(
+            all_articles[0].title,
+            article1.title
+        )
+        self.assertEqual(
+            all_articles[1].title,
+            article2.title
+        )
